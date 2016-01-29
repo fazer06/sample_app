74fabb85f0907a960005b58f585907a07b664074
fazer06 <shaun.ttre@gmail.com>
2016-01-29 13:35:32 +0000

    Added Bootstrap and some helpers plus tidy up the layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,84 @@ module ApplicationHelper
 		end
 	end
 
+
+    # Change the value below between the quotes.
+	def meta_viewport
+    	"width=device-width, initial-scale=1"
+  	end
+
+    # Change the value below between the quotes.
+	def meta_author
+    	"Website Author. Find me in application_helper.rb"
+  	end
+
+    # Change the value below between the quotes.
+	def meta_description
+		"Add your website description here. Find me in application_helper.rb"
+	end
+
+    # Change the value below between the quotes.
+	def meta_keywords
+		"Add your keywords here. Find me in application_helper.rb"
+	end
+
+	# http://api.rubyonrails.org/classes/ActionView/Helpers/DateHelper.html#method-i-date_select
+	def get_date_time() 
+		my_date = Time.now
+  	end
+
+	# Copyright Notice, with year range
+	def copyright_notice_year_range(start_year)
+		# In case the input was not a number (nil.to_i will return a 0)
+		start_year = start_year.to_i
+
+		# Get the current year from the system
+		current_year = Time.new.year
+
+		# When the current year is more recent than the start year, return a string 
+		# of a range (e.g., 2010 - 2012). Alternatively, as long as the start year 
+		# is reasonable, return it as a string. Otherwise, return the current year 
+		# from the system.
+		if current_year > start_year && start_year > 2000
+			"#{start_year} - #{current_year}"
+		elsif start_year > 2000
+			"#{start_year}"
+		else
+			"#{current_year}"
+		end
+	end
+
+	# Output what season it is based on the month
+	def get_season()
+		time = Time.new
+
+		if(time.month >= 3) && (time.month <= 5)
+					"Yeah it is Spring"
+			elsif (time.month > 5) && (time.month <= 8)
+					"Everyone Loves Summer"
+			elsif (time.month > 8) && (time.month <= 10)
+					"Put on Your Coat because Autumn is here"
+			else
+					"Yuck, it's now Winter"
+		end
+	end
+
+  	# A random welcome message for the home page jumbotron
+	def get_random_message()
+		opening = [ "Developer Portfolio",
+            		"My Rails 4 Portfolio",
+            		"Hello World" ]
+
+		middle = [  "Built from the Rails Tutorial",
+            		"Rails Apps Are Great",
+            		"Rails Twitter clone App" ]
+
+		ending = [  "Contact us if you need help.",
+            		"We are here to server you. ",
+            		"Call us if you need to 412-555-1212."]
+
+		"#{opening[rand(3)]}"
+
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,29 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+
 	# Define which page is the root of the application
 	root 'static_pages#home'
+	
 	# By using get we arrange for the route to respond to a GET request
-	# get a page at some address http://localhost:3000/static_pages/home
-	get 'static_pages/help'
-	get 'static_pages/about'
+	# get a page at some address http://localhost:3000/help
+	# VERB	/url  	  =>  	'controller#action'
+	get     'help'    =>	'static_pages#help'
+	get     'about'   =>	'static_pages#about'
+	get     'signup'  =>	'users#new'
+
+	# The users resource lets us respond to the RESTful URLs
+
+	# Prefix    Verb    URI Pattern       Controller#Action
+	# users     GET     /users            users#index
+	#           POST    /users            users#create
+	# new_user  GET     /users/new        users#new
+	# edit_user GET     /users/:id/edit   users#edit
+	# user      GET     /users/:id        users#show
+	#           PUT     /users/:id        users#update
+	#           PATCH   /users/:id        users#update
+	#           DELETE  /users/:id        users#destroy
+
+	resources :users
 
 end
