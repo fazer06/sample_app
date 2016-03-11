d49ab4ec7393d76b62b161e5ad272193b93053b4
fazer06 <shaun.ttre@gmail.com>
2016-03-11 15:06:19 +0000

    Add password resets & email configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,8 @@
 #  activation_digest :string
 #  activated         :boolean          default(FALSE)
 #  activated_at      :datetime
+#  reset_digest      :string
+#  reset_sent_at     :datetime
 #
 # Indexes
 #
@@ -22,7 +24,7 @@
 
 class User < ActiveRecord::Base
 
-	attr_accessor :remember_token, :activation_token
+	attr_accessor :remember_token, :activation_token, :reset_token
 	# downcase the email and username attributes before saving the user
 	#before_save { self.email = email.downcase }
 	before_save   :downcase_email
@@ -103,6 +105,23 @@ class User < ActiveRecord::Base
 	# Sends activation email.
 	def send_activation_email
 		UserMailer.account_activation(self).deliver_now
+	end
+
+	# Sets the password reset attributes.
+	def create_reset_digest
+		self.reset_token = User.new_token
+		update_attribute(:reset_digest,  User.digest(reset_token))
+		update_attribute(:reset_sent_at, Time.zone.now)
+	end
+
+	# Sends password reset email.
+	def send_password_reset_email
+		UserMailer.password_reset(self).deliver_now
+	end
+
+	# Returns true if a password reset has expired.
+	def password_reset_expired?
+		reset_sent_at < 2.hours.ago
 	end
 
 	private
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,11 @@
 #                         PUT    /users/:id(.:format)                    users#update
 #                         DELETE /users/:id(.:format)                    users#destroy
 # edit_account_activation GET    /account_activations/:id/edit(.:format) account_activations#edit
+#         password_resets POST   /password_resets(.:format)              password_resets#create
+#      new_password_reset GET    /password_resets/new(.:format)          password_resets#new
+#     edit_password_reset GET    /password_resets/:id/edit(.:format)     password_resets#edit
+#          password_reset PATCH  /password_resets/:id(.:format)          password_resets#update
+#                         PUT    /password_resets/:id(.:format)          password_resets#update
 #
 
 Rails.application.routes.draw do
@@ -50,5 +55,7 @@ Rails.application.routes.draw do
 	#           DELETE  /users/:id        users#destroy
 
 	resources :users
+	# resources for account activations and password resets
 	resources :account_activations, only: [:edit]
+	resources :password_resets,     only: [:new, :create, :edit, :update]
 end
